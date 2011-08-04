366682c62f0bb4b987094f0f02f0e8a24848e475
k1LoW <k1lowxb@gmail.com>
2011-08-04 18:30:41 +0900

    Update 00_common.el

--- dot-emacs/.emacs.d/conf/00_common.el
+++ dot-emacs/.emacs.d/conf/00_common.el
@@ -6,3 +6,24 @@
 (prefer-coding-system 'utf-8-unix)
 (set-keyboard-coding-system 'utf-8)
 (setq default-buffer-file-coding-system 'utf-8)
+
+;;自動カラー設定
+(require 'font-lock)
+(global-font-lock-mode t)
+
+;;対応する括弧をハイライト表示させる
+(setq scroll-conservatively 1)
+(show-paren-mode 1)
+
+;;行番号を表示
+(line-number-mode 1)
+;;列番号を表示
+(column-number-mode 1)
+;;メニューバーを非表示
+(menu-bar-mode -1)
+
+;;C-vなどでページ移動があってもカーソル位置を変化させない
+(setq scroll-preserve-screen-position t)
+
+;;選択範囲に色がつくように変更
+(transient-mark-mode t)
