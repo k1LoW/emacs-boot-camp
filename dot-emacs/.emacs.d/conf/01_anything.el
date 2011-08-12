8841d31e677bb732127a3ed859c22ef23f94cae8
k1LoW <k1lowxb@gmail.com>
2011-08-12 16:47:27 +0900

    Add anything-c-moccur

--- dot-emacs/.emacs.d/conf/01_anything.el
+++ dot-emacs/.emacs.d/conf/01_anything.el
@@ -12,6 +12,25 @@
    "*anything*"))
 
 
+;;; anything-c-moccur
+(require 'color-moccur)
+(require 'anything-c-moccur)
+
+(setq moccur-split-word t)
+;; 複数の検索語や、特定のフェイスのみマッチ等の機能を有効にする
+;; 詳細は http://www.bookshelf.jp/soft/meadow_50.html#SEC751
+(setq moccur-split-word t)
+;; migemoがrequireできる環境ならmigemoを使う
+(when (require 'migemo nil t)
+  (setq moccur-use-migemo t))
+
+;; カスタマイズ可能変数の設定(M-x customize-group anything-c-moccur でも設定可能)
+(setq anything-c-moccur-anything-idle-delay 0.2   ; `anything-idle-delay'
+      anything-c-moccur-higligt-info-line-flag t  ; `anything-c-moccur-dmoccur'などのコマンドでバッファの情報をハイライトする
+      anything-c-moccur-enable-auto-look-flag t   ; 現在選択中の候補の位置を他のwindowに表示する
+      anything-c-moccur-enable-initial-pattern t) ; `anything-c-moccur-occur-by-moccur'の起動時にポイントの位置の単語を初期パターンにする
+
+
 
 
 
