42d98332e8f86c9c6fb439e6db066135e884ff1c
k1LoW <k1lowxb@gmail.com>
2011-08-12 16:01:54 +0900

    Add anything-main()

--- dot-emacs/.emacs.d/conf/01_anything.el
+++ dot-emacs/.emacs.d/conf/01_anything.el
@@ -1,1 +1,40 @@
 (require 'anything-config)
+
+(defun anything-main ()
+  "my main anything-command"
+  (interactive)
+  (anything-other-buffer
+   (list anything-c-source-buffers+
+         anything-c-source-recentf
+         anything-c-source-files-in-current-dir+)
+   "*anything*"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
